089799767448396e77deb2baac765d8970d0c424
roger medico <rmedico@nrs-group.com>
2023-07-21 20:05:48 +0200

    js tweak

--- resources/js/video.js
+++ resources/js/video.js
@@ -2,11 +2,8 @@
     const video = document.getElementById('end-video');
 
     if (video) {
-        video.addEventListener('canplay', () => {
-            console.log('inside event')
-            setTimeout(() => {
-                video.play();
-            }, 5000);
-        });
+        setTimeout(() => {
+            video.play();
+        }, 5000);
     }
 })();
