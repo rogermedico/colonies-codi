00683d5ca0fd1a3b9059379a2a8ec1c977ef1d93
roger medico <rmedico@nrs-group.com>
2023-08-03 13:43:22 +0200

    use specific codes to seed

--- database/seeders/CodeSeeder.php
+++ database/seeders/CodeSeeder.php
@@ -14,10 +14,39 @@ class CodeSeeder extends Seeder
      */
     public function run(): void
     {
+        //$this->randomCodes();
+        $this->specificCodes();
+    }
+
+    private function specificCodes()
+    {
+        Folder::all()
+            ->each(function (Folder $folder) {
+                $codes = Code::SPECIFIC_CODES[$folder->name];
+
+                shuffle($codes);
+
+                $i = 0;
+
+                foreach ($codes as $code) {
+                    Code::factory()
+                        ->create([
+                            'folder_id' => $folder->id,
+                            'code' => $code,
+                            'order' => $i,
+                        ]);
+
+                    $i = $i + 1;
+                }
+            });
+    }
+
+    private function randomcodes()
+    {
         $numberOfCodes = count(Folder::NAMES) - 1;
 
         Folder::all()
-            ->each(function (Folder $folder) use ($numberOfCodes){
+            ->each(function (Folder $folder) use ($numberOfCodes) {
                 for ($i = 0; $i < $numberOfCodes; $i++) {
                     Code::factory()
                         ->create([
