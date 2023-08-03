59e3b5d4528c476a224a8912d6da9563ca65205a
roger medico <rmedico@nrs-group.com>
2023-08-03 13:43:43 +0200

    activated final page imgs and more

--- resources/views/activated.blade.php
+++ resources/views/activated.blade.php
@@ -5,11 +5,16 @@
         ⚡ Seguretat dels generadors ⚡
     </h1>
 
-    <h2 class="text-center">
+    <h2 class="text-center mb-3">
         Activant generadors: <span id="activation-countdown">0h 0m 12s</span>
     </h2>
 
-    <video id="end-video" class="delay fade-in" data-delay="15000" controls loop preload="auto">
+    <div id="no-autorefresh" class="container text-center mt-3">
+        <img id="activated-ko" src="{{asset('storage/pikachu_ko.png')}}" class="rounded-5">
+        <img id="activated-ok" src="{{asset('storage/pikachu_ok.gif')}}" class="rounded-5 d-none">
+    </div>
+
+    {{-- <video id="end-video" class="delay fade-in" data-delay="15000" controls loop preload="auto">
         <source src="{{asset('storage/end.mp4')}}" type="video/mp4">
-    </video>
+    </video> --}}
 @endsection
